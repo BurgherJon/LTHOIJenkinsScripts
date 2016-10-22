3bbfa512a664754ad6cca3c9027ce1f418bf49d1
Jonathan Cavell <Jonathan Cavell>
2016-10-22 13:35:13 -0400

    Adding the files that need to be moved.

---
+++ test/Constants.java
@@ -0,0 +1,15 @@
+package com.lthoi;
+
+/**
+ * 
+ * This is the test file.
+ * 
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "424262593373-ktjrme8l5aeqlgo61dei3lesj30m8fop.apps.googleusercontent.com";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
